9c7e1ab45553e14f5df1e40099d2dd3076c5c3eb
AbbyGoose <157957828+AbbyGoose@users.noreply.github.com>
2024-10-26 19:27:41 +0300

    Изменил всё

--- logic.py
+++ logic.py
@@ -13,6 +13,8 @@ class Pokemon:
         self.name = self.get_name()
         self.moves = self.get_moves()
 
+        self.hp = randint(150, 250)
+        self.power = randint(30, 60)
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -47,7 +49,7 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}. Здоровье: {self.hp}, сила: {self.power}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
@@ -56,5 +58,37 @@ class Pokemon:
     def show_moves(self):
         return f'Первая способность - {self.moves}'
 
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            shield = randint(1,5)
+            if shield == 1:
+                return "Покемон-волшебник применил щит в сражении"
+            else:
+                if enemy.hp > self.power:
+                    enemy.hp -= self.power
+                    return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+                else:
+                    enemy.hp = 0
+                    return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+        else:
+            if enemy.hp > self.power:
+                enemy.hp -= self.power
+                return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+            else:
+                enemy.hp = 0
+                return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
+class Wizard(Pokemon):
+    def info(self):
+        return 'У тебя покемон-волшебник'
+        
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        end = super().attack(enemy)
+        self.power -= super_power
+        return end + f"\nБоец применил супер-атаку силой:{super_power} "
 
+    def info(self):
+        return 'У тебя покемон-боец' + super().info(self)
